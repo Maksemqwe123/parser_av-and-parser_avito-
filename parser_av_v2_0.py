bddb34147af9ae8bc5cd19ddbf097ac50ad0ee93
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2022-11-13 14:29:36 +0200

    Initial commit

--- parser_av_v2_0.py
+++ parser_av_v2_0.py
@@ -6,67 +6,69 @@ from bs4 import BeautifulSoup
 start_time = time.time()
 
 items = []
-urls = []
 years = []
 cashes = []
-mileages = []
 locations = []
+urls = []
 
 
-def get_info(html_source):
-    global items, urls, years, mileages, locations, cashes
+class Parser:
+    def __init__(self, pages: range):
+        self.pages = pages
+        self.items = items
+        self.years = years
+        self.cashes = cashes
+        self.locations = locations
+        self.urls = urls
 
-    page_info = BeautifulSoup(html_source, 'html.parser')
+        self._get_html()
 
-    car_names = page_info.find_all('a', class_='listing-item__link')
-    for name in car_names:
-        items.append(name.text)
-        urls.append(f"https://cars.av.by/bmw{name['href']}")
+    def _get_html(self):
+        for page in self.pages:
+            if page == 1:
+                url = 'https://cars.av.by/filter?brands[0][brand]=8&sort=4'
+            else:
+                url = f'https://cars.av.by/filter?page={page}&brands[0][brand]=8&sort=4'
 
-    items_cashes = page_info.find_all('div', class_='listing-item__priceusd')
-    for cash in items_cashes:
-        cashes.append(cash.text.replace('&nbsp;', ' '))
+            response = requests.get(url=url)
 
-    years_html = page_info.find_all('div', class_="listing-item__params")
-    for year in years_html:
-        for i in year:
-            years.append(i.text)
-            break
+            try:
+                assert response.status_code == 200
+                html_source = response.text
+                self._get_info(html_source)
+            except AssertionError as e:
+                print(f'ERROR: {repr(e)}')
+                print(response.status_code)
 
-    mileages_html = page_info.find_all('div', class_="listing-item__params")
-    for mileage in mileages_html:
-        for a in mileage:
-            mileages.append(a.text)
-            break
+    def _get_info(self, html_source):
+        pages_info = BeautifulSoup(html_source, 'html.parser')
 
-    locations_html = page_info.find_all('div', class_="listing-item__location")
-    for location in locations_html:
-        locations.append(location.text)
+        car_names = pages_info.find_all('a', class_='listing-item__link')
+        for name in car_names:
+            self.items.append(name.text)
+            self.urls.append(f"https://cars.av.by/bmw{name['href']}")
 
+        items_cashes = pages_info.find_all('div', class_='listing-item__priceusd')
+        for cash in items_cashes:
+            self.cashes.append(cash.text.replace('&nbsp;', ' '))
 
-def get_html(pages: range):
-    for page in pages:
-        if page == 1:
-            url = 'https://cars.av.by/filter?brands[0][brand]=8&sort=4'
-        else:
-            url = f'https://cars.av.by/filter?page={page}&brands[0][brand]=8&sort=4'
-        response = requests.get(url=url)
+        years_html = pages_info.find_all('div', class_="listing-item__params")
+        for year in years_html:
+            self.years.append(year.text)
 
-        try:
-            assert response.status_code == 200
-            html_source = response.text
-            get_info(html_source)
-        except AssertionError as e:
-            print(f'ERROR: {repr(e)}')
-            print(response.status_code)
+        # mileages_html = pages_info.find_all('div', class_="listing-item__params")
+        # for mileage in mileages_html:
+        #     self.mileages.append(mileage.text)
+
+        locations_html = pages_info.find_all('div', class_="listing-item__location")
+        for location in locations_html:
+            self.locations.append(location.text)
 
 
 if __name__ == '__main__':
-    pages = range(1, 2)
-    get_html(pages)
-    all_info = list(zip(items, years, cashes, mileages, locations, urls))
+    parse = Parser(range(1, 2))
+    all_info = list(zip(items, years, cashes, locations, urls))
     for i in all_info:
-        print(f'\n\nМарка: {i[0]}, Год: {i[1]}, Цена: {i[2]}, Пробег: {i[3]}, Локация: {i[4]} Ссылка: {i[-1]}\n\n')
-    print(len(all_info))
+        print(f'\n\nМарка: {i[0]}, Год: {i[1]}, Цена: {i[2]}, Локация: {i[4]} Ссылка: {i[-1]}\n\n')
     end_time = time.time() - start_time
     print(f'\nВремя работы: {end_time} секунд')
--- parser_avito_v1_0.py
+++ parser_avito_v1_0.py
@@ -6,58 +6,60 @@ from random import randrange
 start_time = time.time()
 
 items = []
-urls = []
 text = []
 cashes = []
-images = []
+urls = []
 
 
-def get_info(html_source):
-    global items, urls, text, cashes, images
+class Parser:
+    def __init__(self, pages: range):
+        self.pages = pages
+        self.text = text
+        self.cashes = cashes
+        self.urls = urls
 
-    page_info = BeautifulSoup(html_source, 'html.parser')
+        self._get_html()
 
-    furniture_objects = page_info.find_all('a', class_="iva-item-titleStep-pdebR")
-    for objects in furniture_objects:
-        items.append(objects.text)
-        urls.append(f"https://www.avito.ru/sankt-peterburg/mebel_i_interer{objects['href']}")
+    def _get_html(self):
+        for page in self.pages:
+            if page == 1:
+                url = 'https://www.avito.ru/sankt-peterburg/mebel_i_interer'
+            else:
+                url = f'https://www.avito.ru/sankt-peterburg/mebel_i_interer?p={page}'
+            response = requests.get(url=url)
 
-    items_cashes = page_info.find_all('div', class_='body-priceRow-h69TD')
-    for cashes in items_cashes:
-        cashes.append(cashes.text)
+            time.sleep(randrange(2, 5))
 
-    text_html = page_info.find_all('div', class_='iva-item-descriptionStep-C0ty1')
-    for text in text_html:
-        text.append(text.text)
+            try:
+                assert response.status_code == 200
+                html_source = response.text
+                self._get_info(html_source)
+            except AssertionError as e:
+                print(f'ERROR: {repr(e)}')
+                print(response.status_code)
 
-    time.sleep(randrange(2, 5))
+    def _get_info(self, html_source):
 
+        pages_info = BeautifulSoup(html_source, 'html.parser')
 
-def get_html(pages: range):
-    for page in pages:
-        if page == 1:
-            url = 'https://www.avito.ru/sankt-peterburg/mebel_i_interer?p=1'
-        else:
-            url = f'https://www.avito.ru/sankt-peterburg/mebel_i_interer?p={page}'
-        response = requests.get(url=url)
+        furniture_objects = pages_info.find_all('a', class_="iva-item-titleStep-pdebR")
+        for object in furniture_objects:
+            items.append(object.text)
+            self.urls.append(f"https://www.avito.ru/sankt-peterburg/mebel_i_interer{object['href']}")
 
-        time.sleep(randrange(2, 5))
+        items_cashes = pages_info.find_all('div', class_='body-priceRow-h69TD')
+        for cashe in items_cashes:
+            self.cashes.append(cashe.text)
 
-        try:
-            assert response.status_code == 200
-            html_source = response.text
-            get_info(html_source)
-        except AssertionError as e:
-            print(f'ERROR: {repr(e)}')
-            print(response.status_code)
+        text_html = pages_info.find_all('div', class_='iva-item-descriptionStep-C0ty1')
+        for text in text_html:
+            self.text.append(text.text)
 
 
 if __name__ == '__main__':
-    pages = range(1, 2)
-    get_html(pages)
-    all_info = list(zip(items, cashes, text, images, urls))
+    parser = Parser(range(1, 2))
+    all_info = list(zip(items, cashes, text, urls))
     for i in all_info:
-        print(f'\n\n Мебель: {i[0]}, Цена: {i[1]}, Текст: {i[2]} Ссылка: {i[-1]} \n\n')
-    print(len(all_info))
+        print(f'\n\n Мебель: {items[0]}, Цена: {cashes[1]}, Текст: {text[2]},  Ссылка: {urls[-1]} \n\n')
     end_time = time.time() - start_time
     print(f'\nВремя работы: {end_time} ceкунд')
